4e5cd0296b3c4146a8753d588fe4516276db8f6e
Brent Pedersen <bpederse@gmail.com>
2020-07-09 19:09:11 -0600

    wip hist

--- src/seqcoverpkg/background.nim
+++ src/seqcoverpkg/background.nim
@@ -1,5 +1,6 @@
 import argparse
 import strformat
+import math
 import os
 import d4
 import tables
@@ -7,47 +8,59 @@ import ./utils
 import algorithm
 import times
 
-proc summarize_block(d4s: var seq[D4], chrom:string, start:uint32, stop:uint32, percentiles: seq[float], outs:var seq[D4], vals: var seq[seq[int32]]) =
+
+proc summarize_block(d4s: var seq[D4], chrom:string, start:uint32, stop:uint32, percentiles: seq[float], outs:var seq[D4], vals: var seq[seq[int32]], out_vals: var seq[seq[int32]]) =
   # extract data for each sample from start to stop, calculate per-base
   # percentiles and write to outs.
+  # vals and out_vals just let us avoid allocations
   for i in 0..vals.high:
     if vals[i].len != int(stop) - int(start):
-      vals[i].setLen(int(stop) - int(stop))
+      vals[i].setLen(int(stop) - int(start))
       echo "changing length:", vals[i].len, " start:", start, " stop:", stop
 
     d4s[i].values(chrom, start, vals[i])
+
+  if out_vals[0].len != vals[0].len:
+    for i in 0..out_vals.len:
+      out_vals[i].setLen(vals[0].len)
 
   let n_samples = d4s.len.float
 
   # we just keep here which sample we'll stop at when getting to each
   # percentile
-  var int_sample = newSeq[int](percentiles.len)
+  var int_sample = newSeqUninitialized[uint32](percentiles.len)
   for i, p in percentiles:
-    int_sample[i] = max(0, int(n_samples * p - 0.5))
+    int_sample[i] = uint32(max(0, int(n_samples * p - 0.5)))
 
-  var out_vals = newSeq[seq[int32]](outs.len)
-  for i in 0..outs.high:
-    out_vals[i] = newSeqUninitialized[int32](vals[0].len)
-
-  var column = newSeq[int32](d4s.len)
-
-  var H : array[256, uint32]
+  var H : array[1024, uint32]
   for i in 0..vals[0].high:
     zeroMem(H[0].addr.pointer, sizeof(H[0]) * H.len)
     # fill the histogram
     for j in 0..d4s.high:
       H[min(H.high, vals[j][i])].inc
 
-    for k, isamp in int_sample:
-      var tot = 0'u32
-      var m = 0
-      for ns in H:
-        tot += ns
-        if tot > isamp.uint32:
-          #echo "cutoff:", n, ".. ", m, " vs ", column[n], " tot:", tot
-          break
-        m.inc
-      out_vals[k][i] = m.int32 # column[n]
+    var cutoff_index = 0
+    var isamp = int_sample[cutoff_index]
+
+    # extract the percentiles from the histogram.
+    # this is faster than doing binary search for each value as we iterate over
+    # array only once and most values are near the left end.
+    var high_set = false
+    block cumsum_block:
+      var cumsum:uint32
+      for k, n in H:
+        cumsum += n
+        # use while in case we had a chunk of samples that span some percentiles.
+        while cumsum > isamp:
+          out_vals[cutoff_index][i] = k
+          cutoff_index += 1
+          if cutoff_index > int_sample.high:
+            high_set = true
+            break cumsum_block
+          isamp = int_sample[cutoff_index]
+    # if we ran out of samples, just set it to highest value.
+    if not high_set:
+      out_vals[^1][i] = H.len.int32
 
   for i in 0..out_vals.high:
     outs[i].write(chrom, start, out_vals[i])
@@ -68,33 +81,36 @@ proc generate_backgrounds(d4s: var seq[D4], output_dir: string, percentiles: seq
 
   var outs = newSeq[D4](percentiles.len)
   for i in 0..<percentiles.len:
-    doAssert outs[i].open(&"{outputDir}/seqcover_p{percentiles[i]}", mode="w")
+    doAssert outs[i].open(&"{outputDir}/seqcover_p{percentiles[i]}.d4", mode="w")
     outs[i].set_chromosomes(chroms)
   var vals = newSeq[seq[int32]](d4s.len)
   for v in vals.mitems: v.setLen(block_size)
+
+  var out_vals = newSeq[seq[int32]](outs.len)
+  for i in 0..outs.high:
+    out_vals[i] = newSeqUninitialized[int32](vals[0].len)
+
 
   for (chrom, length) in info.pairs:
     d4s.check_same_lengths(chrom, length)
     let t0 = cpuTime()
     var j = 0
     for i in countup(0'u32, length, block_size):
-      d4s.summarize_block(chrom, i, min(i + block_size, length), pctiles, outs, vals)
-      if j mod 5 == 0:
+      if j mod 10 == 0 and i > 0:
         echo &"{chrom}:{min(i + blocksize, length)} time:{cpuTime() - t0:.2f} bases/second: {min(i + blocksize, length).float / (cpuTime() - t0):.0f}"
+      d4s.summarize_block(chrom, i, min(i + block_size, length), pctiles, outs, vals, out_vals)
       j.inc
-      if i > 40_000_000: break
-    break
 
   for o in outs.mitems:
     o.close()
 
 proc main() =
   let p = newParser("seqcover background"):
-    option("-o", "--output-dir", help="directory for output", default="d4-backgrounds")
+    option("-o", "--output-dir", help="directory for output", default="seqcover-backgrounds")
     arg("samples", nargs= -1, help="d4 files or a glob of d4 files to generate background")
 
 
-  let percentiles = @[10, 50, 90]
+  let percentiles = @[5, 10, 50, 90, 95]
   var argv = commandLineParams()
   if len(argv) > 0 and argv[0] == "report":
     argv = argv[1..argv.high]
